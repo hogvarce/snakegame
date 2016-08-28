a69cfbe6ad235fc074a59b0499bbf804b7ecbeb0
hogvarce <hogvarce@gmail.com>
2016-08-28 18:05:05 +0300

    Добавлен хвост змее

--- src/main/java/ixn/shakegame/SnakeGame.java
+++ src/main/java/ixn/shakegame/SnakeGame.java
@@ -1,6 +1,6 @@
 package ixn.shakegame;
 
-import ixn.shakegame.objects.Apple;
+import ixn.shakegame.objects.Rabbit;
 import ixn.shakegame.objects.Snake;
 
 import javax.swing.*;
@@ -9,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.geom.Arc2D;
 import java.awt.image.BufferedImage;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -24,7 +25,7 @@ public class SnakeGame extends JPanel implements ActionListener{
     public static final int SPEED = 5;
 
     Snake s = new Snake(10, 10, 9, 10);
-    Apple a = new Apple((int)(Math.random()*WIDTH), (int)(Math.random()*HEIGHT));
+    Rabbit r = new Rabbit((int)(Math.random()*WIDTH), (int)(Math.random()*HEIGHT));
     Timer t = new Timer(1000/SPEED, this);
 
     public SnakeGame() {
@@ -48,22 +49,36 @@ public class SnakeGame extends JPanel implements ActionListener{
 //        }
 
         for (int d = 0; d < s.length; d++) {
-            g.setColor(color(200,150,0));
-            g.fillRect(s.snakeX[d]*SCALE, s.snakeY[d]*SCALE, SCALE, SCALE);
+            ImageIcon imageIcon;
+            Image image;
+            int max = s.length - 1;
+            if (d == 0) {
+                imageIcon = new ImageIcon("src/main/resource/images/snakehead.png"); // load the image to a imageIcon
+                image = imageIcon.getImage(); // transform it
+                g.drawImage(ScaledImage(image, SCALE, SCALE, (s.direction * 90)), s.snakeX[d] * SCALE, s.snakeY[d] * SCALE, this);
+            } else if (d == max) {
+                imageIcon = new ImageIcon("src/main/resource/images/snakeend.png"); // load the image to a imageIcon
+                image = imageIcon.getImage(); // transform it
+                g.drawImage(ScaledImage(image, SCALE, SCALE, (s.direction * 90)), s.snakeX[d] * SCALE, s.snakeY[d] * SCALE, this);
+            } else {
+                g.setColor(color(200,150,0));
+                g.fillRect(s.snakeX[d]*SCALE, s.snakeY[d]*SCALE, SCALE, SCALE);
+            }
         }
 
-        ImageIcon imageIcon = new ImageIcon("src/main/resource/images/Rabbit.png"); // load the image to a imageIcon
+        ImageIcon imageIcon = new ImageIcon("src/main/resource/images/rabbit2.gif"); // load the image to a imageIcon
         Image image = imageIcon.getImage(); // transform it
-        g.drawImage(ScaledImage(image, SCALE, SCALE), a.posX*SCALE+1, a.posY*SCALE+1, this);
+        g.drawImage(ScaledImage(image, SCALE, SCALE, 0), r.posX*SCALE+1, r.posY*SCALE+1, this);
     }
 
     public Color color(int red, int green, int blue) {
         return new Color(red, green, blue);
     }
 
-    private static Image ScaledImage(Image img, int w, int h) {
+    private static Image ScaledImage(Image img, int w, int h, double r) {
         final BufferedImage bufferedImage = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
         final Graphics2D graphics2D = bufferedImage.createGraphics();
+        graphics2D.rotate(Math.toRadians(r), w/2, h/2);
         graphics2D.setComposite(AlphaComposite.Src);
         graphics2D.drawImage(img, 0, 0, w, h, null);
         graphics2D.dispose();
@@ -73,14 +88,7 @@ public class SnakeGame extends JPanel implements ActionListener{
     public static void main(String[] args) throws MalformedURLException {
         JFrame f = new JFrame("Snake");
 
-//        ImageIcon imageIcon = new ImageIcon("src/main/resource/images/Rabbit.png"); // load the image to a imageIcon
-//        Image image = imageIcon.getImage(); // transform it
-//        imageIcon = new ImageIcon(ScaledImage(image, 120, 120));
-
-//        JLabel label = new JLabel(imageIcon);
-//        f.getContentPane().add(label);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        f.pack();
         f.setResizable(false);
         f.setSize(WIDTH*SCALE+SCALE/4, HEIGHT*SCALE+SCALE);
         f.setLocationRelativeTo(null);
@@ -95,13 +103,13 @@ public class SnakeGame extends JPanel implements ActionListener{
             if( s.snakeX[i] == s.snakeX[0] && s.snakeY[i] == s.snakeY[0] )
                 s.length = 2;
         }
-        if ( (s.snakeX[0] == a.posX) && (s.snakeY[0] == a.posY) ) {
+        if ( (s.snakeX[0] == r.posX) && (s.snakeY[0] == r.posY) ) {
             s.length++;
-            a.setRandomPosition();
+            r.setRandomPosition();
         }
         for(int i = 1; i < s.length; i++) {
-            if( s.snakeX[i] == a.posX && s.snakeY[i] == a.posY )
-                a.setRandomPosition();
+            if( s.snakeX[i] == r.posX && s.snakeY[i] == r.posY )
+                r.setRandomPosition();
         }
         repaint();
     }
